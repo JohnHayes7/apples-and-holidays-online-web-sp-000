e9ba50c2b7aeac7f5789a4d966b882e1d3865657
John Hayes <johnnyhayes7@gmail.com>
2019-02-01 15:38:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,7 +84,6 @@ def all_supplies_in_holidays(holiday_hash)
   
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-      holiday_array = holiday.keys
       supply_array = holiday.values
       holiday_strings = holiday.keys.to_s 
       
