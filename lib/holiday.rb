6ea2001bd3c7c5fa20dc65b891e0e48b8aa3fd8f
John Hayes <johnnyhayes7@gmail.com>
2019-02-01 16:05:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,11 +84,10 @@ def all_supplies_in_holidays(holiday_hash)
   
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-      split_array = 
       cap_array = []
       
       holiday.keys.collect do |i|
-      split_array << i.to_s.split("_")
+      split_holiday = i.to_s.split("_")
       end 
         split_array.collect do |j|
           cap_array << j.join(" ").capitalize
