f131a44c0bf0fb307136c2764c92bc645b6a258f
John Hayes <johnnyhayes7@gmail.com>
2019-02-01 15:59:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -86,6 +86,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
       split_array = []
       cap_array = []
+      
       holiday.keys.collect do |i|
       split_array << i.to_s.split("_")
       end 
