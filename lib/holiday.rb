1b8dc812e1881d093f225e51cb35ff23b0fbc7c1
John Hayes <johnnyhayes7@gmail.com>
2019-02-01 15:17:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,8 +87,8 @@ def all_supplies_in_holidays(holiday_hash)
       holiday_array = holiday.keys
       supply_array = holiday.values
   
-     holiday_array.each do |i|
-     end
+       holiday_array.each do |i|
+       end
       # puts "  #{day[0].to_s.capitalize}: #{day[1].join(", ")}"
       binding.pry
   end
