de878981c144e2482e5b5c14382129d4cc949965
John Hayes <johnnyhayes7@gmail.com>
2019-02-01 16:22:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -99,7 +99,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.each do |season, holiday|
     holiday.each do |day, supplies|
-      if supplies.include?
+      if supplies.include? ==
     end
   end
     
